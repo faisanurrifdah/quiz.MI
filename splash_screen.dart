f9c19803da2e80ce7efa98691503db88949e249c
faisanurrifdah <148935230+faisanurrifdah@users.noreply.github.com>
2025-04-13 18:44:58 +0700

    Update splash_screen.dart

--- splash_screen.dart
+++ splash_screen.dart
@@ -1,1 +1,53 @@
+import 'package:flutter/material.dart';
+import 'halaman_quiz.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 93, 157, 186),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/icon_ya.png',
+              width: 350,
+              height: 350,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Quiz.MI",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HalamanQuiz()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: const Text("START", style: TextStyle(fontSize: 18)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
